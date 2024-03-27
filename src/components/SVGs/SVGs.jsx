6e737eb4d4ed84f0ca239222692cee2bdd9779f7
simokitkat <simokitkat@yahoo.com>
2024-03-28 02:25:17 +0300

    feat: add mobile responsivness

--- src/components/SVGs/SVGs.jsx
+++ src/components/SVGs/SVGs.jsx
@@ -1,6 +1,12 @@
 export function ArrowUp() {
   return (
-    <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="40"
+      height="40"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 40 40"
+      fill="none"
+    >
       <g fill="none" fillRule="evenodd">
         <circle fill="#303030" cx="20" cy="20" r="20" />
         <path stroke="#FFF" strokeWidth="2" d="M14 23l6-6 6 6" />
